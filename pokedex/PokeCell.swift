dfd927122e9aa954f5dd4fab3825794c960c8957
Andrew Ingram <andrewingram@Andrews-MacBook-Pro.local>
2017-06-06 19:57:08 -0400

    added code for collection view

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,33 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Andrew Ingram on 2017-06-06.
+//  Copyright © 2017 Sabring. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+        
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLabel.text = self.pokemon.name.capitalized
+        thumbImage.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+}
---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Andrew Ingram on 2017-06-06.
+//  Copyright © 2017 Sabring. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init (name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
